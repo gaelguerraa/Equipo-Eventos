885852436fcd5357cf05d0c1d88bff82215f7507
gaelguerra <gaelguerra05@icloud.com>
2025-03-20 23:10:32 -0700

    Update EventoDTO.java

--- SistemaEventosNegocio/src/main/java/DTO_Negocio/EventoDTO.java
+++ SistemaEventosNegocio/src/main/java/DTO_Negocio/EventoDTO.java
@@ -4,10 +4,51 @@
  */
 package DTO_Negocio;
 
+import java.util.List;
+
 /**
  *
  * @author gael_
  */
 public class EventoDTO {
+    private String nombreEvento;
+    private List<String> etiquetas;
+    private String descripcionEvento;
+
+    public EventoDTO() {
+    }
+    
+    
+
+    public EventoDTO(String nombreEvento, List<String> etiquetas, String descripcionEvento) {
+        this.nombreEvento = nombreEvento;
+        this.etiquetas = etiquetas;
+        this.descripcionEvento = descripcionEvento;
+    }
+
+    public String getNombreEvento() {
+        return nombreEvento;
+    }
+
+    public void setNombreEvento(String nombreEvento) {
+        this.nombreEvento = nombreEvento;
+    }
+
+    public List<String> getEtiquetas() {
+        return etiquetas;
+    }
+
+    public void setEtiquetas(List<String> etiquetas) {
+        this.etiquetas = etiquetas;
+    }
+
+    public String getDescripcionEvento() {
+        return descripcionEvento;
+    }
+
+    public void setDescripcionEvento(String descripcionEvento) {
+        this.descripcionEvento = descripcionEvento;
+    }
+    
     
 }
