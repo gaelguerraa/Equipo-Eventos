7d6e06ff7577f604fecff773f02e75cb2ca0079d
sansanty03 <138645645+sansanty03@users.noreply.github.com>
2025-03-21 10:20:44 -0700

    s

--- SistemaEventosNegocio/src/main/java/sistemaEventosNegocio/IFachadaEventos.java
+++ SistemaEventosNegocio/src/main/java/sistemaEventosNegocio/IFachadaEventos.java
@@ -4,10 +4,27 @@
  */
 package sistemaEventosNegocio;
 
+import DTO_Negocio.EventoDTO;
+import ObjetosNegocio.Evento;
+import ObjetosNegocio.Mapa;
+import ObjetosNegocio.Usuario;
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  *
  * @author gael_
  */
 public interface IFachadaEventos {
+    public abstract List<Usuario> encontrarContactos(Usuario usu);
+    
+    public abstract List<Usuario> seleccionarInvitados(Usuario usu);
+    
+    public abstract String SeleccionarUbicaion(Mapa map);
+    
+    public abstract LocalDateTime SeleccionarFechaHora();
+    
+    public abstract Evento AgregarDetalles(EventoDTO eventoDTO);
+    
     
 }
